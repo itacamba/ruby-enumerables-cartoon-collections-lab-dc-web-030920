8358d54ae6a2eaa5cca3ba70bbdf5b25754c039b
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-25 05:16:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,18 +16,5 @@ end
 def find_the_cheese(array)# code an argument here ["gouda","trash","lamb"]
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array_counter = 0
-  while array_counter < array.length do
-    cheese_counter = 0
-    while cheese_counter < cheese_types.length do
-      if array[array_counter] == cheese_types[cheese_counter]
-        result = cheese_types[cheese_counter]
-      else
-        cheese_counter += 1
-      end
-      cheese_counter += 1
-    end
-    array_counter += 1
-  end
-  result
+  array.include?(cheese_types)
 end
