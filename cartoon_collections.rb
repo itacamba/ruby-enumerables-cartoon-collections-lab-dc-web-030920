95864d7b6b2f3928cc2e2eadfb8cc71350076972
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-25 04:58:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,13 +13,13 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   planeteer_calls.any? {|x| x.length > 4}
 end
 
-def find_the_cheese(cheese)# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0
   while i < cheese_types.length do
-    if cheese == cheese_types[i]
-      result = cheese
+    if array[i] == cheese_types[i]
+      result = cheese_types[i]
     end
   i += 1
   end
