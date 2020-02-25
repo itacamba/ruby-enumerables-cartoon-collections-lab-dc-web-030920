f4ef64ca947815563142dc78864ebada949a9150
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-25 04:49:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   planeteer_calls.any? {|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
