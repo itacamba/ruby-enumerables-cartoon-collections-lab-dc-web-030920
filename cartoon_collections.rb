a35743577b05e0c7832bbb617a4f03c471457f8f
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-25 04:46:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,8 @@ def long_planeteer_calls(planeteer_calls)# code an argument here
   while i < planeteer_calls.length do
     if planeteer_calls[i].length > 4
       result = true
+    else
+     i += 1
     end
     i += 1
   end
